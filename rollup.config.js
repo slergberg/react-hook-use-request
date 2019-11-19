2412680d3a30abe3e0862b8b5ffb5d65d0d69050
Rodrigo Scomação do Nascimento <rodrigo.sc.na@gmail.com>
2019-11-19 10:59:00 -0300

    Fix build script

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
-import nodeResolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
-import replace from 'rollup-plugin-replace'
 import commonjs from 'rollup-plugin-commonjs'
+import json from 'rollup-plugin-json'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import replace from 'rollup-plugin-replace'
 import { terser } from 'rollup-plugin-terser'
 
 import packageDefinition from './package.json'
@@ -9,7 +10,7 @@ import packageDefinition from './package.json'
 const { NODE_ENV } = process.env
 
 const config = {
-  input: 'src/index.js',
+  input: 'src/index.ts',
   external: Object.keys(packageDefinition.peerDependencies || {}),
   output: {
     format: 'umd',
@@ -23,6 +24,9 @@ const config = {
     babel({
       exclude: '**/node_modules/**',
       runtimeHelpers: true,
+    }),
+    json({
+      exclude: '**/node_modules/**',
     }),
     replace({
       'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
