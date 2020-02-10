dbc712fface82a7c68dacafbfabffefc87271e05
Rodrigo Scomação do Nascimento <rodrigo.sc.na@gmail.com>
2020-02-10 12:53:08 -0300

    Update project settings

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from 'rollup-plugin-commonjs'
 import json from 'rollup-plugin-json'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import replace from 'rollup-plugin-replace'
+import visualizer from 'rollup-plugin-visualizer'
 import { terser } from 'rollup-plugin-terser'
 
 import packageDefinition from './package.json'
@@ -20,6 +21,7 @@ const config = {
     },
   },
   plugins: [
+    visualizer({ filename: 'stats/index.html' }),
     nodeResolve(),
     babel({
       exclude: '**/node_modules/**',
