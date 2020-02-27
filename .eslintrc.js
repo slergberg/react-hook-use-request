f08e6b768c019ac506d2c93710427ae53e9fe462
Ches1422 <61529391+Ches1422@users.noreply.github.com>
2020-02-27 15:37:07 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,9 +10,9 @@ module.exports = {
   parser: require.resolve('babel-eslint'),
   rules: {
     'import/prefer-default-export': 'off',
-    'jsx-a11y/media-has-caption': 'off',
+    'jsx-a11y/media-has-caption': 'on',
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
     'react/jsx-fragments': ['error', 'element'],
-    'react/require-default-props': 'off',
+    'react/require-default-props': 'on',
   },
 }
